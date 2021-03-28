d7145593267edab42b89ed70d9448e1231fd131d
Mehmet Can Aygün <mca199667@gmail.com>
2021-03-28 19:24:16 +0300

    Add Start button in Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="home-page">Home</div>;
+  return (
+    <div className="home-page">
+      <div className="home-container">
+        <div className="info">
+          <i className="fas fa-info-circle"></i>
+          <p>
+            Başlat butonuna bastığında kelimeler rastgele bir şekilde gelecek.
+            Kelimenin anlamını ve örnek cümlesini görmek için kartın üzerine
+            tıkla.
+          </p>
+        </div>
+        <div className="start-btn-container">
+          <button>
+            <i className="fas fa-play"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
