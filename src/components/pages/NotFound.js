4d94281197efaff5ee6d812fdbdf1815d9d8fa86
Mehmet Can Aygün <mca199667@gmail.com>
2021-04-04 18:55:27 +0300

    Add small styling for 404 page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div className="not-found-page">Not Found</div>;
+  return (
+    <div
+      className="not-found-page"
+      style={{
+        paddingTop: "20px",
+        textAlign: "center",
+      }}
+    >
+      <h2>404</h2>
+      <p>Böyle bir sayfa yok :(</p>
+    </div>
+  );
 };
 
 export default NotFound;
