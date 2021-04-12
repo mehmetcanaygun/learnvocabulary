eb8b28f5a6d5600a7cf7cf896e1a60640253560b
Mehmet Can Aygün <mca199667@gmail.com>
2021-04-12 15:31:17 +0300

    Add reset button on the navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { reset } from "../../redux/actions";
 
-const Navbar = () => {
+const Navbar = ({ reset }) => {
+  const resetProgress = () => {
+    if (window.confirm("İlerlemeni sıfırlamak istediğine emin misin?")) {
+      reset();
+    }
+  };
+
   return (
     <header className="navbar f-center">
       <div className="logo f-center">LV</div>
+      <button className="reset-btn" onClick={() => resetProgress()}>
+        <i className="fas fa-undo-alt"></i> Sıfırla
+      </button>
     </header>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  reset: PropTypes.func.isRequired,
+};
+
+export default connect(null, { reset })(Navbar);
